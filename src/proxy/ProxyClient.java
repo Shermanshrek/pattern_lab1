34c28bc9c715d3cbb1421ba3f431d9a865bfa5a9
Gevorgyan David <ref.ddk@inbox.ru>
2026-02-17 16:51:38 +0400

    little fix

---
+++ src/proxy/ProxyClient.java
@@ -0,0 +1,33 @@
+package proxy;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public record ProxyClient(Socket socket) implements Runnable {
+    @Override
+    public void run() {
+        try (DataInputStream in = new DataInputStream(socket.getInputStream());
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            double a = in.readDouble();
+            double b = in.readDouble();
+
+            System.out.printf("Получены числа %f и %f%n", a, b);
+
+            double res = a * b;
+
+            out.writeDouble(res);
+            out.flush();
+            System.out.println("Отправлен результат: " + res);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException _) {
+
+            }
+        }
+    }
+}
--- src/proxy/ProxyServer.java
+++ src/proxy/ProxyServer.java
@@ -1,7 +1,5 @@
 package proxy;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,38 +12,10 @@ public class ProxyServer {
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Подключился клиент: " + socket.getInetAddress());
-                new Thread(new ClientHandler(socket)).start();
+                new Thread(new ProxyClient(socket)).start();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-    private record ClientHandler(Socket socket) implements Runnable {
-
-        @Override
-            public void run() {
-                try (DataInputStream in = new DataInputStream(socket.getInputStream());
-                     DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-                    double a = in.readDouble();
-                    double b = in.readDouble();
-
-                    System.out.printf("Получены числа %f и %f%n", a, b);
-
-                    double res = a * b;
-
-                    out.writeDouble(res);
-                    out.flush();
-                    System.out.println("Отправлен результат: " + res);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    try {
-                        socket.close();
-                    } catch (IOException _) {
-
-                    }
-                }
-            }
-        }
 }
